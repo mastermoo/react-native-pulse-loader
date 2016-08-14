feef98783effcda08ae54edf8cb58125a61c029c
Yousef Kamawall <yousef.kamawall@gmail.com>
2016-08-14 18:47:48 +0200

    initial commit

---
+++ PulseLoader.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { View, Image, TouchableOpacity, Animated, Easing } from 'react-native';
+import Pulse from './Pulse';
+
+
+export default class LocationPulseLoader extends React.Component {
+	constructor(props) {
+		super(props);
+	
+		this.state = {
+			circles: []
+		};
+
+		this.counter = 1;
+		this.setInterval = null;
+		this.anim = new Animated.Value(1);
+	}
+
+	componentDidMount() {
+		this.setCircleInterval();
+	}
+
+	setCircleInterval() {
+		this.setInterval = setInterval(this.addCircle.bind(this), this.props.interval);
+		this.addCircle();
+	}
+
+	addCircle() {
+		this.setState({ circles: [...this.state.circles, this.counter] });
+		this.counter++;
+	}
+
+	onPressIn() {
+		Animated.timing(this.anim, {
+			toValue: this.props.pressInValue,
+			duration: this.props.pressDuration,
+			easing: this.props.pressInEasing,
+		}).start(() => clearInterval(this.setInterval));
+	}
+
+	onPressOut() {
+		Animated.timing(this.anim, {
+			toValue: 1,
+			duration: this.props.pressDuration,
+			easing: this.props.pressOutEasing,
+		}).start(this.setCircleInterval.bind(this));
+	}
+
+	render() {
+		const { size, avatar, avatarBackgroundColor, interval } = this.props;
+
+		return (
+			<View style={{
+				flex: 1,
+				backgroundColor: 'transparent',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}>
+				{this.state.circles.map((circle) => (
+					<Pulse
+						key={circle}
+						{...this.props}
+					/>
+				))}
+
+				<TouchableOpacity
+					activeOpacity={1}
+					onPressIn={this.onPressIn.bind(this)}
+					onPressOut={this.onPressOut.bind(this)}
+					style={{
+						transform: [{
+							scale: this.anim
+						}]
+					}}
+				>
+					<Image
+						source={{ uri: avatar }}
+						style={{
+							width: size,
+							height: size,
+							borderRadius: size/2,
+							backgroundColor: avatarBackgroundColor
+						}}
+					/>
+				</TouchableOpacity>
+			</View>
+		);
+	}	
+}
+
+LocationPulseLoader.propTypes = {
+  interval: React.PropTypes.number,
+  size: React.PropTypes.number,
+  pulseMaxSize: React.PropTypes.number,
+  avatar: React.PropTypes.string.isRequired,
+  avatarBackgroundColor: React.PropTypes.string,
+  pressInValue: React.PropTypes.number,
+  pressDuration: React.PropTypes.number,
+  borderColor: React.PropTypes.string,
+  backgroundColor: React.PropTypes.string,
+  getStyle: React.PropTypes.func,
+};
+
+LocationPulseLoader.defaultProps = {
+  interval: 2000,
+  size: 100,
+  pulseMaxSize: 250,
+  avatar: undefined,
+  avatarBackgroundColor: 'white',
+  pressInValue: 0.8,
+  pressDuration: 150,
+  pressInEasing: Easing.in,
+  pressOutEasing: Easing.in,
+  borderColor: '#D8335B',
+  backgroundColor: '#ED225B55',
+  getStyle: undefined,
+};
+
---
+++ README.md
@@ -0,0 +1,50 @@
+# react-native-pulse-loader
+tinder-like loader for your react native app
+
+![react-native-pulse-loader demo](http://i.giphy.com/l0MYz2cMbOryuyPZu.gif)
+
+### Installation
+```bash
+npm i react-native-pulse-loader --save
+```
+
+### Usage
+
+First, require it from your app's JavaScript files with:
+```bash
+import PulseLoader from 'react-native-pulse-loader';
+```
+
+### Example
+
+```js
+import React from 'react';
+import PulseLoader from 'react-native-pulse-loader';
+
+class App extends React.Component {
+  render() {
+    return (
+      <LocationPulseLoader
+        avatar={'https://scontent-fra3-1.cdninstagram.com/t51.2885-15/e35/11429705_386886401514376_550879228_n.jpg'}
+      />
+    );
+  }
+}
+```
+
+
+### API
+
+| Property       | Type          | Default             | Description |
+| -------------  |:-------------:|:------------:       | ----------- |
+| interval       | number        | 2000                | action buttons visible or not
+| size           | number        | 100                 | width and height of the avatar
+| pulseMaxSize   | number        | 250                 | maximum size of the pulse in the background
+| avatar         | string        | undefined           | **required** avatar url to display
+| pressInValue   | number        | 0.8                 | should be between 0 and 1. scale of the avatar, when pressed in
+| pressDuration  | number        | 150                 | duration of the scale animation
+| pressInEasing  | Easing        | Easing.in           | easing type of the press in animation
+| pressOutEasing | Easing        | Easing.out          | easing type of the press out animation
+| borderColor    | string        | '#D8335B'           | border color of the pulse
+| backgroundColor| string        | '#ED225B55'         | background color of the pulse
+| getStyle       | function      | undefined           | override the styling of the pulse. gets as parameter the Animated variable. e.g. (anim) => ({ backgroundColor: 'yellow' })
